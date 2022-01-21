fb6fe52dcebcb30bc25636306a49dfd440828b98
Demanghon Jérôme <demanghonj@gmail.com>
2022-01-21 15:38:14 +0100

    implementing of currentuser endpoint

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { ConfigurationError } from "../errors/configuration-error";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there!");
+router.get("/api/users/currentuser", (req:Request, res: Response) => {
+    let currentUser = null;
+    if(req.session?.jwt){
+        const jwtUser = req.session.jwt;
+        try{
+            const payload  = jwt.verify(jwtUser, process.env.JWT_KEY!)
+            currentUser = payload;
+        }catch(err){
+            throw new ConfigurationError([{key: "JWT_KEY", message: "Invalid key"}]);
+        }
+    }
+
+    res.status(200).send({currentUser});
+
+
 });
 
 export {router as currentUserRouter}
