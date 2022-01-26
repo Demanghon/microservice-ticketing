fb76def866245c095b70bea9d58c834e20a2bfa1
Demanghon Jérôme <demanghonj@gmail.com>
2022-01-26 15:28:08 +0100

    Add nats and base event

--- common/src/index.ts
+++ common/src/index.ts
@@ -12,3 +12,9 @@ export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
 export * from './config/config-checker'
+
+export * from './events/base-listener'
+export * from './events/base-publisher'
+export * from './events/ticket-created-event'
+export * from './events/ticket-updated-event'
+export * from './events/subjects'
